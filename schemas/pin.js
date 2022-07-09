03d10ed0409818ccb3d482109a0c6bb888563725
Shasa <shasa.thuo@gmail.com>
2022-07-09 17:20:11 +0300

    initial commit

---
+++ schemas/pin.js
@@ -0,0 +1,15 @@
+export default {
+    name: 'pin',
+    title: 'pin',
+    type: 'document',
+    fields:[
+        {name:'title', title:'Title', type:'string'},
+        {name:'about', title:'About', type:'string'},
+        {name:'destination', title:'Destination', type:'url'},
+        {name:'category', title:'Category', type:'string'},
+        {name:'image', title:'Image', type:'image',options:{hotspot:true}},
+        {name:'userId', title:'User Id', type:'string'},
+        {name: 'save', title:'Save', type:'array', of:[{type:'save'}]},
+        {name: 'comments', title:'Comments', type:'array', of:[{type:'comment'}]}
+    ]
+}
